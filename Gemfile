33a9020ee7b80e96bf604d65879e667d5ffd4d26
Jayde Moors <jaydemoors@gmail.com>
2013-04-05 23:01:01 -0700

    Add Paperclip gem

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.12'
 gem 'jquery-rails'
 gem 'devise'
 gem 'simple_form'
+gem "paperclip", "~> 3.0"
 
 group :production do
 	gem 'pg'
@@ -11,6 +12,7 @@ end
 
 group :development, :test do
 	gem 'sqlite3'
+	gem 'annotate'
 end
 
 # Gems used only for assets and not required
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,26 @@
+# == Schema Information
+#
+# Table name: pins
+#
+#  id          :integer          not null, primary key
+#  description :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
 class Pin < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :image
+ 
+
+
   validates :description, presence: true
+  validates :user_id, presence: true
+	validates_attachment :image, presence: true,
+												content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+												size: { less_than: 5.megabytes }			
 
   belongs_to :user
-  validates :user_id, presence: true
+  has_attached_file :image, styles: { medium: "320x240>"}
+
 end
